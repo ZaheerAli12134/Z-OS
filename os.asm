fb02170a773cf2b96903c5c7549ad0efb2c838cb
ZaheerAli12134 <zaheermirali121@gmail.com>
2025-10-20 23:26:26 +0100

    inital commit, basic bootloader with Hello World message

---
+++ os.asm
@@ -0,0 +1,21 @@
+[org 0x7c00]
+mov ax, 0
+mov ds, ax
+mov si, msg
+jmp loop
+main:
+    jmp $
+
+loop:
+    lodsb
+    test al, al
+    jz main
+    mov ah, 0x0e
+    int 0x10
+    jmp loop
+
+
+
+msg: db 'Hello World!',0
+times 510-($-$$) db 0       
+dw 0xAA55 

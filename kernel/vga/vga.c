b43a6a0def077e4bae387b6448f9834c13c3b1c9
ZaheerAli12134 <zaheermirali121@gmail.com>
2025-11-04 16:06:33 +0000

    fixes

--- kernel/vga/vga.c
+++ kernel/vga/vga.c
@@ -1,49 +1,46 @@
-#include <vga.h>
+#include "vga.h"
 
-uint16_t column = 0;
-uint16_t line = 0;
 static uint16_t* const vga = (uint16_t*)0xB8000;
 static const uint8_t color = (COLOR8_LIGHT_GREY | (COLOR8_BLACK << 4));
+static uint16_t column = 0;
+static uint16_t line = 0;
 
-void scroll(){
-    for(uint16_t y = 1; y<height ; y++){
-        for(uint16_t x = 0; x<width;x++){
-            vga[(y-1)*width + x] = vga[y*width + x];
+void scroll(void) {
+    for (uint16_t y = 1; y < height; y++) {
+        for (uint16_t x = 0; x < width; x++) {
+            vga[(y - 1) * width + x] = vga[y * width + x];
         }
     }
 
-    for(uint16_t x = 0; x<width;x++){
-        vga[(height-1)*width+x] = ' ';
+    for (uint16_t x = 0; x < width; x++) {
+        vga[(height - 1) * width + x] = ' ' | ((uint16_t)color << 8);
     }
-};
+}
 
-void newLine(){
-    if(line < height -1){
+void newLine(void) {
+    if (line < height - 1) {
         line++;
         column = 0;
-
-    }
-    else{
+    } else {
         scroll();
         column = 0;
     }
-};
+}
 
-void print(const char* s){
-    while(*s){
+void print(const char* s) {
+    while (*s) {
         char c = *s++;
-        
-        if(c == '\n'){
+
+        if (c == '\n') {
             newLine();
-            break;
-        }
-        else{
-            if(column == width){
+        } else {
+            if (column >= width)
                 newLine();
-            }
-            vga[line*width + (column++)] = c | color;
-            break;
+
+            vga[line * width + column] = (uint16_t)c | ((uint16_t)color << 8);
+            column++;
         }
     }
-
 }
+
+
